5d47e011c29dc2d225732c49a9caacba8b96d45f
siroj05 <rojudin123@gmail.com>
2023-03-14 16:47:04 +0700

    delete bug

--- app/routes/expenses/$id.tsx
+++ app/routes/expenses/$id.tsx
@@ -1,7 +1,15 @@
+import { redirect } from "react-router";
+import { deleteitem } from "~/data/expense.server";
 export default function DetailItem(){
     return(
         <main>
             <div>hello</div>
         </main>
     )
+}
+
+export async function action({params}:any){
+    const itemId = params.id;
+    await deleteitem(itemId);
+    return redirect('expenses');
 }
